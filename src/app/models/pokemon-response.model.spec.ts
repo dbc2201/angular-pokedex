1c52516289095b16efe219c69ca7e8666730cd2b
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 02:19:08 +0530

    added model class for one pokemon response

---
+++ src/app/models/pokemon-response.model.spec.ts
@@ -0,0 +1,7 @@
+import { PokemonResponse } from './pokemon-response.model';
+
+describe('PokemonResponse', () => {
+  it('should create an instance', () => {
+    expect(new PokemonResponse()).toBeTruthy();
+  });
+});
