02a9e750a2e5d6817b807cf5ee276acd532c4514
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 02:29:58 +0530

    completed pokemon model class

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,2 +1,45 @@
 export class Pokemon {
+	private id: number;
+	private name: string;
+	private type: string;
+	private imageURL: string;
+
+	constructor(id: number, name: string, type: string, imageURL: string) {
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.imageURL = imageURL;
+	}
+
+	public get pokemonID(): number {
+		return this.id;
+	}
+
+	public get pokemonName(): string {
+		return this.name;
+	}
+
+	public get pokemonType(): string {
+		return this.type;
+	}
+
+	public get pokemonImageURL(): string {
+		return this.imageURL;
+	}
+
+	public set pokemonID(id: number) {
+		this.id = id;
+	}
+
+	public set pokemonName(name: string) {
+		this.name = name;
+	}
+
+	public set pokemonType(type: string) {
+		this.type = type;
+	}
+
+	public set pokemonImageURL(imageURL: string) {
+		this.imageURL = imageURL;
+	}
 }
