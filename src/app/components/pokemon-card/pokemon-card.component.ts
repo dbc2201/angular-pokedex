d1f6f2dc2392f715382c67aa4467075640800ca0
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 02:43:36 +0530

    sent pokemon data from list manager to card

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Component({
-  selector: 'app-pokemon-card',
-  templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.css']
+	selector: 'app-pokemon-card',
+	templateUrl: './pokemon-card.component.html',
+	styleUrls: ['./pokemon-card.component.css'],
 })
 export class PokemonCardComponent {
-
+	@Input()
+	pokemon: Pokemon | undefined;
 }
