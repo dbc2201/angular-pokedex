e10a3657ed5f37eac594bc7a2780713634d6906b
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-26 23:59:59 +0530

    added form element

--- src/app/components/search-pokemon/search-pokemon.component.ts
+++ src/app/components/search-pokemon/search-pokemon.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-pokemon',
-  templateUrl: './search-pokemon.component.html',
-  styleUrls: ['./search-pokemon.component.css']
+	selector: 'app-search-pokemon',
+	templateUrl: './search-pokemon.component.html',
+	styleUrls: ['./search-pokemon.component.css'],
 })
-export class SearchPokemonComponent {
-
-}
+export class SearchPokemonComponent {}
