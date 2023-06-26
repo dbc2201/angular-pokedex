fed2d295e031042d05b52d537f4f1c4c0d0ad96b
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 00:05:42 +0530

    added material form field module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchPokemonComponent } from './components/search-pokemon/search-pokemon.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent, SearchPokemonComponent],
@@ -23,6 +24,7 @@ import { SearchPokemonComponent } from './components/search-pokemon/search-pokem
 		MatSidenavModule,
 		MatIconModule,
 		MatListModule,
+		MatFormFieldModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
