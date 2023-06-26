b54334fc9e46b77e156fc206d36597e8db83f093
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 01:23:27 +0530

    added dummy array and showed cards

--- src/app/components/pokemon-list-manager/pokemon-list-manager.component.ts
+++ src/app/components/pokemon-list-manager/pokemon-list-manager.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-pokemon-list-manager',
-  templateUrl: './pokemon-list-manager.component.html',
-  styleUrls: ['./pokemon-list-manager.component.css']
+	selector: 'app-pokemon-list-manager',
+	templateUrl: './pokemon-list-manager.component.html',
+	styleUrls: ['./pokemon-list-manager.component.css'],
 })
 export class PokemonListManagerComponent {
-
+	// dummy array to simulate the number of cards
+	pokemonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
