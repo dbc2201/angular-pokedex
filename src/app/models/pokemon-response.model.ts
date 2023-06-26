5841e502bfc7944db6b79be785dbf2575725a5f6
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-27 02:22:33 +0530

    created pokemon response class

--- src/app/models/pokemon-response.model.ts
+++ src/app/models/pokemon-response.model.ts
@@ -1,2 +1,25 @@
 export class PokemonResponse {
+	private name: string;
+	private url: string;
+
+	constructor(name: string, url: string) {
+		this.name = name;
+		this.url = url;
+	}
+
+	public get pokemonName(): string {
+		return this.name;
+	}
+
+	public set pokemonName(name: string) {
+		this.name = name;
+	}
+
+	public get pokemonURL(): string {
+		return this.url;
+	}
+
+	public set pokemonURL(url: string) {
+		this.url = url;
+	}
 }
